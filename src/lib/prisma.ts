7b32beff159884ae9c4b355f82e871e3d4cd3659
marketingpuntofarma-a11y <marketing.puntofarma@gmail.com>
2026-04-28 11:18:12 -0300

    Revert to default DATABASE_URL setup

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,8 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasourceUrl: process.env.POSTGRES_PRISMA_URL || "postgresql://dummy:dummy@dummy:5432/dummy"
-})
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
